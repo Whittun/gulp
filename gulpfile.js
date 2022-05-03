44991f24a5c6bd154896bf7167168ed43b141cfd
Whittun <Collapse.23@yandex.ru>
2022-05-03 09:49:52 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'));
+const rename = require('gulp-rename')
+const del = require('del')
+const cleanCSS = require('gulp-clean-css')
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
+const concat = require('gulp-concat')
+
+
+
+const paths = {
+    styles: {
+        src: 'src/styles/**/*.scss',
+        dest: 'dist/css/'
+    },
+    scripts: {
+        src: 'src/scripts/**/*.js',
+        dest: 'dist/js/'
+    }
+}
+
+function clean() {
+    return del(['dist'])
+}
+
+function styles() {
+    return gulp.src(paths.styles.src)
+        .pipe(sass())
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(cleanCSS())
+        .pipe(rename({
+            basename: 'style',
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest(paths.styles.dest))
+}
+
+function scripts() {
+    return gulp.src(paths.scripts.src, {
+        sourcemaps: true
+    })
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(concat(('main.min.js')))
+    .pipe(gulp.dest(paths.scripts.dest))
+} 
+
+function watch() {
+    gulp.watch(paths.styles.src, styles)
+    gulp.watch(paths.scripts.src, scripts)
+}
+
+const build = gulp.series(clean, gulp.parallel(styles, scripts), watch)
+
+exports.clean = clean
+exports.styles = styles
+exports.watch = watch
+exports.build = build
+exports.default = build 
